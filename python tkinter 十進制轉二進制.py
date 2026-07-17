0f01e5aa3f1b22b31e33fe45743e5a04ac6e1cb9
tohham <jc1000110@gmail.com>
2026-07-17 15:41:00 +0800

    Add files via upload

--- python tkinter 十進制轉二進制.py
+++ python tkinter 十進制轉二進制.py
@@ -8,22 +8,22 @@ level = 2             # 2進位的轉換
 def two(num, level):
     rem = ""          # 用來暫存 除法產生的餘數，最後會反向組合出最終結果的字串
     
-    # 短除法原理 如果商數大於 0 時 持續進行除法運算
+    # 短除法原理 如果商數大於 0 時就持續進行除法運算
     while num > 0:
-        # 1. 算出當前數值除以基底的餘數
-        # 2. 將餘數轉為字串後，串接到 rem 字串的「最前面」（因為短除法餘數要由下往上讀）
+        # 算出當前數值除以基底的餘數
+        # 將餘數轉成字串後 接到 rem 字串 的最前面（因為短除法餘數所以 由下往上讀）
         rem = (str(int((num % int(level))))) + (rem)
         
-        # 進行整除，更新商數以進行下一輪計算
+        # 進行整除後更新商數來進行下一次的計算
         num = (int(num) // int(level))
         
-        print(num)    # 在終端機印出目前的商數（偵錯用）
-        print(rem)    # 在終端機印出目前累積的餘數字串（偵錯用）
+        print(num)    # 印出目前的商數
+        print(rem)    # 印出目前累積的餘數字串
         
-        show.config(text=rem)  # 將即時計算出的進位字串更新到 GUI 介面的 show 標籤上
+        show.config(text=rem)  # 將即時計算出的進位字串更新到tkinter介面上的 show Label 上
 
 
-# 定義點擊按鈕後觸發的函式
+# 定義點擊按鈕後 所要做的函式
 def get():
     global num        # 宣告使用全域變數 num，以便在函式內修改它
     num = get_text.get()  # 從 GUI 的輸入框（Entry）擷取使用者輸入的字串
@@ -31,28 +31,28 @@ def get():
     two(num, level)   # 呼叫轉換函式，並傳入數值與進位制基底開始計算
 
 
-# --- 以下為 Tkinter 視窗介面配置 ---
+#          下面是 Tkinter 視窗介面
 
 root = tk.Tk()             # 建立 Tkinter 的主視窗物件
 root.geometry("600x600")   # 設定視窗初始大小為寬 600 像素、高 600 像素
 root.title("二進位轉換器")  # 設定視窗左上角顯示的標題
 root.config(bg="pink1")    # 將視窗的背景顏色設為粉紅色（pink1）
 
-# 建立上方的主標題標籤，設定文字內容、字型大小，並融入粉紅色背景
+# 建立標題標籤 和 文字內容 字型大小 粉紅色背景
 title = tk.Label(root, text="   二進位轉換器    \n在下面輸入數字", font=("微軟正黑體", 25), bg="pink1")
-title.pack()               # 將標題標籤放置並對齊到視窗畫面上
+title.pack()               # 放到視窗上
 
-get_text = tk.Entry(root, width=20)  # 建立單行文字輸入框，供使用者輸入十進位數字
-get_text.pack()            # 將輸入框放置到視窗畫面上
+get_text = tk.Entry(root, width=20)  # 建立輸入框後讓使用者輸入十進位數字
+get_text.pack()            # 將他放到視窗畫面上
 
-# 建立執行按鈕，當使用者點選時，會透過 command 參數觸發執行 get 函式
+# 建立按鈕 當點他時 會透過 command 執行 get 函式
 btn = tk.Button(root, text="完成!!!", command=get)
-btn.pack()                 # 將按鈕放置到視窗畫面上
+btn.pack()                 # 把它放置到視窗畫面上
 
-# 建立用來顯示轉換結果的標籤，初始文字為空字串，並設定字型與粉紅背景
+# 建立顯示結果的標籤 一開始設為空字串且設定字型與粉紅背景
 show = tk.Label(root, text="", font=("微軟正黑體", 20), bg="pink1")
-show.pack()                # 將結果標籤放置到視窗畫面上
+show.pack()                # 將標籤放到視窗上
 
-root.mainloop()            # 啟動 Tkinter 的事件監聽大迴圈，讓視窗保持顯示並偵測使用者操作
+root.mainloop()            # 啟動 Tkinter 大迴圈
 
 #以上都次自己寫的
